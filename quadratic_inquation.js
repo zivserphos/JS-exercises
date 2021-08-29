7d2a7ee134279bbc50296932e59ca3f1ec754f55
zivserphos <zivfromisrael@gmail.com>
2021-08-29 14:41:35 +0300

    quadraticEquation
    quadraticEquation return array with 2 result or "wrong input" if the sum in the sum in the sqrt is negative...

---
+++ quadratic_inquation.js
@@ -0,0 +1,15 @@
+function  quadraticEquation(num1,num2,num3)
+{
+    if ((num2*num2) -(4*num1*num3) < 0)
+    {
+        return "wrong input";
+    }
+    var counter1 = (num2*-1)+(Math.sqrt((num2*num2)-4*num1*num3));
+    var counter2 = (num2*-1)-(Math.sqrt((num2*num2)-4*num1*num3));
+    var denominator =num1*2;
+    var firstResult = counter1/denominator;
+    var secondResult = counter2/denominator;
+    return [firstResult,secondResult];
+}
+
+console.log((quadraticEquation(7,30,5)));
