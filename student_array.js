31c3bf3b2c4853561932fa9b5f251b90fb82613b
zivserphos <zivfromisrael@gmail.com>
2021-08-30 19:37:35 +0300

    find the median
    code that finds and print the median number

--- student_array.js
+++ student_array.js
@@ -15,5 +15,39 @@ function studentsStatistics (arr)
     }
 
     let range = heighstGrade-lowestGrade;
+    heighstGrade =0;
+    var sortedArr = [];
+    let index =0;
+    var arrLength = arr.length
+    for (p=0; p<arrLength; p++)
+    {
+        for(i=0; i<arr.length ;i++)
+        {
+            if (arr[i] > heighstGrade)    
+            {
+                heighstGrade = arr[i];
+                index=i;
+            }   
+            
+        }       
+        arr.splice(index,1)
+        sortedArr.push(heighstGrade)
+        heighstGrade =0;
+        
+    }
+    var median;
+    if (sortedArr.length % 2 === 0)
+        {
+            median = ((sortedArr[sortedArr.length/2-1]+sortedArr[sortedArr.length/2])/2);
+        }
+    else
+        {
+            median = sortedArr[(sortedArr.length-1)/2];
+        }
+
+    console.log(range,median,)
     
 }
+
+
+studentsStatistics([75,83,60,92,70,80]);
